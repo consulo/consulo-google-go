8afa50e979c5a57e9788582192de05596e3d92af
VISTALL <vistall.valeriy@gmail.com>
2014-02-08 22:56:12 +0200

    compilation fixed

--- src/ro/redeul/google/go/ide/structureview/GoStructureView.java
+++ src/ro/redeul/google/go/ide/structureview/GoStructureView.java
@@ -1,36 +1,46 @@
 package ro.redeul.google.go.ide.structureview;
 
+import org.jetbrains.annotations.NotNull;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.StructureViewTreeElement;
 import com.intellij.ide.structureView.TextEditorBasedStructureViewModel;
 import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
 import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * User: jhonny
  * Date: 06/07/11
  */
-public class GoStructureView implements PsiStructureViewFactory {
-    @Override
-    public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
-        return new TreeBasedStructureViewBuilder() {
-            @NotNull
-            public StructureViewModel createStructureViewModel() {
-                return new TextEditorBasedStructureViewModel(psiFile) {
-                    @NotNull
-                    @Override
-                    public StructureViewTreeElement getRoot() {
-                        return new GoStructureViewElement(getPsiFile());
-                    }
-                };
-            }
+public class GoStructureView implements PsiStructureViewFactory
+{
+	@Override
+	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile)
+	{
+		return new TreeBasedStructureViewBuilder()
+		{
+			@Override
+			@NotNull
+			public StructureViewModel createStructureViewModel(Editor editor)
+			{
+				return new TextEditorBasedStructureViewModel(psiFile)
+				{
+					@NotNull
+					@Override
+					public StructureViewTreeElement getRoot()
+					{
+						return new GoStructureViewElement(getPsiFile());
+					}
+				};
+			}
 
-            public boolean isRootNodeShown() {
-                return false;
-            }
-        };
-    }
+			@Override
+			public boolean isRootNodeShown()
+			{
+				return false;
+			}
+		};
+	}
 }
