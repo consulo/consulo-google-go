c3a5b79aa7d6186322e91dd70f7008bd9834cdfa
VISTALL <vistall.valeriy@gmail.com>
2015-03-01 17:21:35 +0300

    compilation

--- src/ro/redeul/google/go/highlight/GoEditorHighlighter.java
+++ src/ro/redeul/google/go/highlight/GoEditorHighlighter.java
@@ -1,12 +1,7 @@
 package ro.redeul.google.go.highlight;
 
-import com.intellij.openapi.editor.HighlighterColors;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
-import com.intellij.openapi.editor.colors.ex.DefaultColorSchemesManager;
-import com.intellij.openapi.editor.colors.impl.EditorColorsSchemeImpl;
-import com.intellij.openapi.editor.colors.impl.ReadOnlyColorsSchemeImpl;
 import com.intellij.openapi.editor.ex.util.LayeredLexerEditorHighlighter;
-import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
@@ -16,9 +11,11 @@ import com.intellij.openapi.vfs.VirtualFile;
  * Date: Jul 24, 2010
  * Time: 3:38:01 AM
  */
-public class GoEditorHighlighter extends LayeredLexerEditorHighlighter {
-    public GoEditorHighlighter(EditorColorsScheme scheme, Project project, VirtualFile virtualFile) {
-        super(new GoSyntaxHighlighter(), scheme);
-    }
+public class GoEditorHighlighter extends LayeredLexerEditorHighlighter
+{
+	public GoEditorHighlighter(EditorColorsScheme scheme, Project project, VirtualFile virtualFile)
+	{
+		super(new GoSyntaxHighlighter(), scheme);
+	}
 
 }
