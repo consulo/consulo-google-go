fa45232a75b172e3e4f63ffb04ee8c4cb8b8a23f
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-03-19 16:16:41 +0400

    reimpl by code

--- plan9/src/main/java/com/plan9/intel/ide/highlighting/DarculaAdditionalTextAttributesProvider.java
+++ plan9/src/main/java/com/plan9/intel/ide/highlighting/DarculaAdditionalTextAttributesProvider.java
@@ -1,9 +1,11 @@
 package com.plan9.intel.ide.highlighting;
 
 import consulo.annotation.component.ExtensionImpl;
-import consulo.colorScheme.AdditionalTextAttributesProvider;
+import consulo.colorScheme.AttributesFlyweightBuilder;
+import consulo.colorScheme.EditorColorSchemeExtender;
 import consulo.colorScheme.EditorColorsScheme;
-
+import consulo.colorScheme.EffectType;
+import consulo.ui.color.RGBColor;
 import jakarta.annotation.Nonnull;
 
 /**
@@ -11,16 +13,35 @@ import jakarta.annotation.Nonnull;
  * @since 03-Jul-22
  */
 @ExtensionImpl
-public class DarculaAdditionalTextAttributesProvider implements AdditionalTextAttributesProvider {
-  @Nonnull
-  @Override
-  public String getColorSchemeName() {
-    return EditorColorsScheme.DARCULA_SCHEME_NAME;
-  }
+public class DarculaAdditionalTextAttributesProvider implements EditorColorSchemeExtender {
+    @Nonnull
+    @Override
+    public String getColorSchemeId() {
+        return EditorColorsScheme.DARCULA_SCHEME_NAME;
+    }
 
-  @Nonnull
-  @Override
-  public String getColorSchemeFile() {
-    return "/colorscheme/AsmIntelDarcula.xml";
-  }
+    @Override
+    public void extend(Builder builder) {
+        builder.add(AsmIntelSyntaxHighlightingColors.IDENTIFIER, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0x20, 0x99, 0x9D))
+            .build());
+
+        builder.add(AsmIntelSyntaxHighlightingColors.PSEUDO_INSTRUCTION, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0x8A, 0x79, 0x9B))
+            .withBoldFont()
+            .build());
+
+        builder.add(AsmIntelSyntaxHighlightingColors.INSTRUCTION, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0xCD, 0xA3, 0xE6))
+            .build());
+
+        builder.add(AsmIntelSyntaxHighlightingColors.REGISTER, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0x9E, 0x61, 0x28))
+            .build());
+
+        builder.add(AsmIntelSyntaxHighlightingColors.LABEL, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0x4E, 0xAD, 0xE5))
+            .withEffect(EffectType.WAVE_UNDERSCORE, null)
+            .build());
+    }
 }
--- plan9/src/main/java/com/plan9/intel/ide/highlighting/DefaultAdditionalTextAttributesProvider.java
+++ plan9/src/main/java/com/plan9/intel/ide/highlighting/DefaultAdditionalTextAttributesProvider.java
@@ -1,9 +1,11 @@
 package com.plan9.intel.ide.highlighting;
 
 import consulo.annotation.component.ExtensionImpl;
-import consulo.colorScheme.AdditionalTextAttributesProvider;
+import consulo.colorScheme.AttributesFlyweightBuilder;
+import consulo.colorScheme.EditorColorSchemeExtender;
 import consulo.colorScheme.EditorColorsScheme;
-
+import consulo.colorScheme.EffectType;
+import consulo.ui.color.RGBColor;
 import jakarta.annotation.Nonnull;
 
 /**
@@ -11,16 +13,22 @@ import jakarta.annotation.Nonnull;
  * @since 03-Jul-22
  */
 @ExtensionImpl
-public class DefaultAdditionalTextAttributesProvider implements AdditionalTextAttributesProvider {
-  @Nonnull
-  @Override
-  public String getColorSchemeName() {
-    return EditorColorsScheme.DEFAULT_SCHEME_NAME;
-  }
+public class DefaultAdditionalTextAttributesProvider implements EditorColorSchemeExtender {
+    @Override
+    public void extend(Builder builder) {
+        builder.add(AsmIntelSyntaxHighlightingColors.INSTRUCTION, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0x20, 0x99, 0x9D))
+            .build());
 
-  @Nonnull
-  @Override
-  public String getColorSchemeFile() {
-    return "/colorscheme/AsmIntelDefault.xml";
-  }
+        builder.add(AsmIntelSyntaxHighlightingColors.LABEL, AttributesFlyweightBuilder.create()
+            .withForeground(new RGBColor(0xDD, 0x67, 0x18))
+            .withAdditionalEffect(EffectType.WAVE_UNDERSCORE, null)
+            .build());
+    }
+
+    @Nonnull
+    @Override
+    public String getColorSchemeId() {
+        return EditorColorsScheme.DEFAULT_SCHEME_NAME;
+    }
 }
